76649375f47bf58ccd026831191478d7e6bc17ad
shaderj <asstvoa@gmail.com>
2026-06-27 16:36:41 +0300

    +verif

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"finally/features/postgres"
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -19,18 +21,36 @@ func main() {
 		panic(err)
 	}
 
-	err = postgres.AddBook(ctx, conn, postgres.Book{
-		Title:     "qwewq",
-		Author:    "asdasd",
-		Review:    nil,
-		Date:      2023,
-		Date_add:  time.Now(),
-		Date_read: nil,
-		Is_read:   false,
-	})
-	if err != nil {
-		panic(err)
+	if Verif() {
+		err := postgres.AddBook(ctx, conn, postgres.Book{
+			Title:     "",
+			Author:    "",
+			Review:    nil,
+			Date:      3333,
+			Date_add:  time.Now(),
+			Date_read: nil,
+			Is_read:   false,
+		})
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	fmt.Println("succed!")
+}
+
+func Verif() bool {
+	ch := make(chan struct{})
+	fmt.Println("yes or no?")
+	go func() {
+		bufio.NewReader(os.Stdin).ReadString('\n')
+		ch <- struct{}{}
+	}()
+
+	select {
+	case <-ch:
+		return true
+	case <-time.After(3 * time.Second):
+		return false
+	}
 }
